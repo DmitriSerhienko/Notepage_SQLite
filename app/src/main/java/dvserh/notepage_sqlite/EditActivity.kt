04e609f193c366496974daae96c5ef18d98aaefa
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-05-15 17:50:38 +0300

    update project

--- app/src/main/java/dvserh/notepage_sqlite/EditActivity.kt
+++ app/src/main/java/dvserh/notepage_sqlite/EditActivity.kt
@@ -2,10 +2,24 @@ package dvserh.notepage_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import dvserh.notepage_sqlite.databinding.EditActivityBinding
 
 class EditActivity : AppCompatActivity() {
+    lateinit var binding: EditActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.edit_activity)
+        binding = EditActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickAddImage(view: View) {
+        binding.mainImageLayout.visibility = View.VISIBLE
+        binding.fbAddImage.visibility = View.GONE
+    }
+
+    fun onClickDeleteImage(view: View) {
+        binding.mainImageLayout.visibility = View.GONE
+        binding.fbAddImage.visibility = View.VISIBLE
     }
 }
