09003886017ebbd0137cd202051607c1c0ad0a9c
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-05-15 11:54:11 +0300

    Initial commit

--- app/src/main/java/dvserh/notepage_sqlite/MainActivity.kt
+++ app/src/main/java/dvserh/notepage_sqlite/MainActivity.kt
@@ -2,10 +2,15 @@ package dvserh.notepage_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dvserh.notepage_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
